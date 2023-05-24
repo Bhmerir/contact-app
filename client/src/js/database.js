f79b5b6d6af08dc186bf956e007ade046e8f15b0
Bahareh <mer_ir@yahoo.com>
2023-05-24 11:07:38 -0700

    do all todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,51 @@
-// TODO: Install the following package:
 import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
-const initdb = async () => {
+const initdb = async () =>
+  openDB('cards', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('cards')) {
+        console.log('cards database already exists');
+        return;
+      }
+      db.createObjectStore('cards', { keyPath: 'id', autoIncrement: true });
+      console.log('cards database created');
+    },
+  });
 
-};
+
 
 
 // TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+    const cardsDb = await openDB('cards', 1);
+    const tx = cardsDb.transaction('cards', 'readwrite');
+    const store = tx.objectStore('cards');
+    const request = store.add({ name: name, home: home, cell: cell, email: email });
+    const result = await request;
+    console.log('data saved to the database', result);
 };
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+    const cardsDb = await openDB('cards', 1);
+    const tx = cardsDb.transaction('cards', 'readonly');
+    const store = tx.objectStore('cards');
+    const request = store.getAll();
+    const result = await request;
+    console.log('data retrieved from the database', result);
+    return result;
 };
 
 // TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+    const cardsDb = await openDB('cards', 1);
+    const tx = cardsDb.transaction('cards', 'readwrite');
+    const store = tx.objectStore('cards');
+    const request = store.delete({id});
+    const result = await request;
+    console.log('data deleted from the database', result);
+    return result;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = () => {
   return {
@@ -11,17 +15,61 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Contact Cards',
+        short_name: 'Cards',
+        description: 'Keep track of contact Cards!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+       
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            
+          },
+        ],
+      }),
     ],
 
-    // TODO: Add the correct modules
     module: {
-
-    }
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
   };
 };
